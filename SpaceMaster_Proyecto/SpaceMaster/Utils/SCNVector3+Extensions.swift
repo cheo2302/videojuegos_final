8b5ec8f97a65d5cf7df3cda89c80ee67a4be5b88
cheo2302 <josetavio.cl@gmail.com>
2025-06-04 12:01:57 +0200

    Add files via upload

---
+++ SpaceMaster_Proyecto/SpaceMaster/Utils/SCNVector3+Extensions.swift
@@ -0,0 +1,17 @@
+import SceneKit
+
+public extension SCNVector3 {
+    static func getRandom(from min: SCNVector3 = SCNVector3(-1, -1, -1), to max: SCNVector3 = SCNVector3(1, 1, 1)) -> SCNVector3 {
+        return SCNVector3Make(Float.getRandom(from: min.x, to: max.x),
+                              Float.getRandom(from: min.y, to: max.y),
+                              Float.getRandom(from: min.z, to: max.z))
+    }
+    
+    static func + (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+        return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+    }
+    
+    static prefix func - (vector: SCNVector3) -> SCNVector3 {
+        return SCNVector3Make(-vector.x, -vector.y, -vector.z)
+    }
+}
